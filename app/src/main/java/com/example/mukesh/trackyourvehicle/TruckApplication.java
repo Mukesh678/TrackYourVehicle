acfa52ecf75322f9c6ee99c8bbed34c6d45d9449
airtel121 <pranav.tank7@gmail.com>
2016-05-09 19:13:26 +0530

    from pranav

--- app/src/main/java/com/example/mukesh/trackyourvehicle/TruckApplication.java
+++ app/src/main/java/com/example/mukesh/trackyourvehicle/TruckApplication.java
@@ -3,9 +3,7 @@ package com.example.mukesh.trackyourvehicle;
 import android.app.Application;
 import android.content.Context;
 
-/**
- * Created by hardik.hirpara on 11/17/2015.
- */
+
 public class TruckApplication extends Application
 {
     private static TruckApplication application = null;
